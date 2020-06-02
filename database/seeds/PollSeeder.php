af3b6b473ba3fb7c3b8677c744841cee561145d4
Nick Verheijen <verheijen.webdevelopment@gmail.com>
2020-06-02 16:03:46 +0200

    Removed test poll from seeder so only statuses remain

--- database/seeds/PollSeeder.php
+++ database/seeds/PollSeeder.php
@@ -55,103 +55,103 @@ class PollSeeder extends Seeder
             ],
         ]);
 
-        //
-        // Test poll
-        //
+        // //
+        // // Test poll
+        // //
 
-        $poll = Poll::create([
-            "user_id" => $user->id,
-            "poll_status_id" => $open->id,
-            "title" => "Test poll",
-            "description" => [
-                "nl" => "Test poll om te proberen",
-                "en" => "Test poll to test everything",
-            ],
-            "published" => true,
-            "public" => true,
-        ]);
+        // $poll = Poll::create([
+        //     "user_id" => $user->id,
+        //     "poll_status_id" => $open->id,
+        //     "title" => "Test poll",
+        //     "description" => [
+        //         "nl" => "Test poll om te proberen",
+        //         "en" => "Test poll to test everything",
+        //     ],
+        //     "published" => true,
+        //     "public" => true,
+        // ]);
 
-        //
-        // Test poll -> Question #1 (closed question - single answer)
-        //
+        // //
+        // // Test poll -> Question #1 (closed question - single answer)
+        // //
 
-        $poll_question = PollQuestion::create([
-            "poll_id" => $poll->id,
-            "open_question" => false,
-            "allow_multiple_answers" => false,
-            "question" => [
-                "nl" => "Welke dag is het vandaag?",
-                "en" => "What day is it today?",
-            ],
-        ]);
+        // $poll_question = PollQuestion::create([
+        //     "poll_id" => $poll->id,
+        //     "open_question" => false,
+        //     "allow_multiple_answers" => false,
+        //     "question" => [
+        //         "nl" => "Welke dag is het vandaag?",
+        //         "en" => "What day is it today?",
+        //     ],
+        // ]);
 
-        $poll_question_answer = PollQuestionAnswer::create([
-            "poll_question_id" => $poll_question->id,
-            "value" => [
-                "en" => "Monday",
-                "nl" => "Maandag",
-            ],
-        ]);
+        // $poll_question_answer = PollQuestionAnswer::create([
+        //     "poll_question_id" => $poll_question->id,
+        //     "value" => [
+        //         "en" => "Monday",
+        //         "nl" => "Maandag",
+        //     ],
+        // ]);
         
-        $poll_question_answer_two = PollQuestionAnswer::create([
-            "poll_question_id" => $poll_question->id,
-            "value" => [
-                "en" => "Another day",
-                "nl" => "Andere dag",
-            ],
-        ]);
+        // $poll_question_answer_two = PollQuestionAnswer::create([
+        //     "poll_question_id" => $poll_question->id,
+        //     "value" => [
+        //         "en" => "Another day",
+        //         "nl" => "Andere dag",
+        //     ],
+        // ]);
 
-        //
-        // Test poll -> Question #2 (closed question - multiple answers)
-        //
+        // //
+        // // Test poll -> Question #2 (closed question - multiple answers)
+        // //
 
-        $poll_question_two = PollQuestion::create([
-            "poll_id" => $poll->id,
-            "open_question" => false,
-            "allow_multiple_answers" => true,
-            "question" => [
-                "nl" => "Welke kleuren heeft je kat?",
-                "en" => "Which colors does you cat have?",
-            ]
-        ]);
+        // $poll_question_two = PollQuestion::create([
+        //     "poll_id" => $poll->id,
+        //     "open_question" => false,
+        //     "allow_multiple_answers" => true,
+        //     "question" => [
+        //         "nl" => "Welke kleuren heeft je kat?",
+        //         "en" => "Which colors does you cat have?",
+        //     ]
+        // ]);
 
-        $poll_question_two_answer = PollQuestionAnswer::create([
-            "poll_question_id" => $poll_question_two->id,
-            "value" => [
-                "en" => "Blue",
-                "nl" => "Blauw",
-            ],
-        ]);
+        // $poll_question_two_answer = PollQuestionAnswer::create([
+        //     "poll_question_id" => $poll_question_two->id,
+        //     "value" => [
+        //         "en" => "Blue",
+        //         "nl" => "Blauw",
+        //     ],
+        // ]);
 
-        $poll_question_two_answer_two = PollQuestionAnswer::create([
-            "poll_question_id" => $poll_question_two->id,
-            "value" => [
-                "en" => "Black",
-                "nl" => "Zwart",
-            ]
-        ]);
+        // $poll_question_two_answer_two = PollQuestionAnswer::create([
+        //     "poll_question_id" => $poll_question_two->id,
+        //     "value" => [
+        //         "en" => "Black",
+        //         "nl" => "Zwart",
+        //     ]
+        // ]);
 
-        $poll_question_two_answer_three = PollQuestionAnswer::create([
-            "poll_question_id" => $poll_question_two->id,
-            "value" => [
-                "en" => "White",
-                "nl" => "Wit",
-            ],
-        ]);
+        // $poll_question_two_answer_three = PollQuestionAnswer::create([
+        //     "poll_question_id" => $poll_question_two->id,
+        //     "value" => [
+        //         "en" => "White",
+        //         "nl" => "Wit",
+        //     ],
+        // ]);
 
-        //
-        // Test poll -> Question #3 (Open question)
-        //
+        // //
+        // // Test poll -> Question #3 (Open question)
+        // //
 
-        $poll_question_three = PollQuestion::create([
-            "poll_id" => $poll->id, 
-            "open_question" => true,
-            "allow_multiple_answers" => false,
-            "question" => [
-                "nl" => "Wat vind je tot nu toe van de app?",
-                "en" => "How do you like the application so far?",
-            ]
-        ]);
+        // $poll_question_three = PollQuestion::create([
+        //     "poll_id" => $poll->id, 
+        //     "open_question" => true,
+        //     "allow_multiple_answers" => false,
+        //     "question" => [
+        //         "nl" => "Wat vind je tot nu toe van de app?",
+        //         "en" => "How do you like the application so far?",
+        //     ]
+        // ]);
 
     }
 }
