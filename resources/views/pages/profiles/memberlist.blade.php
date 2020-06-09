88f626c1cf9fce60480aa1f55be558c5643d67e6
Nick Verheijen <verheijen.webdevelopment@gmail.com>
2020-06-09 02:54:53 +0200

    Upgraded memberlist header

--- resources/views/pages/profiles/memberlist.blade.php
+++ resources/views/pages/profiles/memberlist.blade.php
@@ -5,13 +5,29 @@
 @stop
 
 @section("content")
+
+    <!-- Header -->
+    <div id="page-header" class="narrow">
+        <div id="page-header__bg"></div>
+        <div id="page-header__bg-overlay"></div>
+        <div id="page-header__bg-illustration">
+            <div id="bg-illustration__wrapper">
+                <div id="bg-illustration" style="background-image: url({{ asset('storage/images/undraw/meet_the_team.svg') }});"></div>
+            </div>
+        </div>
+        <div id="page-header__content" class="align-left">
+            <div id="page-header__content-wrapper">
+
+                <!-- Title & subtitle -->
+                <h1 id="page-header__title" class="no-margin">@lang("profiles.memberlist_title")</h1>
+                
+            </div>
+        </div>
+    </div>
+
+    <!-- Content -->
     <div class="content-section__wrapper">
         <div class="content-section">
-
-            <!-- Title -->
-            <h1 id="memberlist-title" class="page-title centered">
-                @lang("profiles.memberlist_title")
-            </h1>
 
             <!-- Feedback -->
             @include("partials.feedback")
@@ -27,4 +43,5 @@
 
         </div>
     </div>
+
 @stop
